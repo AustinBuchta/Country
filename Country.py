ce0521dae9e79742aa4deee21195b7a5047f2d1e
badfire <badfire@AMD-AUSTIN>
2024-04-09 22:16:51 -0500

    Bug tested but no rafrance point from PDF.

--- Country.py
+++ Country.py
@@ -1,2 +1,60 @@
+def display_menu():
+    print("=== COUNTRY DICTIONARY ===")
+    print("1. View a country")
+    print("2. Add a country")
+    print("3. Delete a country")
+    print("4. Exit")
 
-s
+def populate_dictionary():
+    countries = {"1": "United States", "2": "United Kingdom", "3": "France"}
+    return countries
+
+def view_country(dictionary):
+    print("Available countries:")
+    for key in dictionary:
+        print(key, "-", dictionary[key])
+    user_input = input("Enter the key of the country you want to view: ")
+    if user_input in dictionary:
+        print("Country:", dictionary[user_input])
+    else:
+        print("Invalid key!")
+
+def add_country(dictionary):
+    key = input("Enter the key for the new country: ")
+    if key in dictionary:
+        print("Country with this key already exists!")
+    else:
+        country = input("Enter the name of the new country: ")
+        dictionary[key] = country
+        print("Country added successfully!")
+
+def delete_country(dictionary):
+    print("Available countries:")
+    for key in dictionary:
+        print(key, "-", dictionary[key])
+    key = input("Enter the key of the country you want to delete: ")
+    if key in dictionary:
+        del dictionary[key]
+        print("Country deleted successfully!")
+    else:
+        print("Invalid key!")
+
+def main():
+    countries_dict = populate_dictionary()
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            view_country(countries_dict)
+        elif choice == "2":
+            add_country(countries_dict)
+        elif choice == "3":
+            delete_country(countries_dict)
+        elif choice == "4":
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid command!")
+
+if __name__ == "__main__":
+    main()
